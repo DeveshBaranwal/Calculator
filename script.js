51329259df7004d3976fb6dc80d551f95b41eb04
DeveshBaranwal <55387003+DeveshBaranwal@users.noreply.github.com>
2021-09-26 00:09:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function insert(num)
+{
+	document.form.textview.value=document.form.textview.value+num;
+}
+
+function equals()
+{
+	var txt=document.form.textview.value;
+	if(txt)
+		document.form.textview.value=eval(txt);
+	else
+		document.form.textview.value='ERROR'
+}
+
+function clean()
+{
+	document.form.textview.value="";
+}
+
